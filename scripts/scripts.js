538202e41d9800828556e930a943e0efae1f3717
ivan sotelo <ivansotelo99@hotmail.com>
2023-02-26 00:23:30 -0300

    Proyecto Final FrontEnd v1.0

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('alert').style.display = "none";
+
+document.getElementById('boton-form').onclick = function(){
+    document.getElementById('alert').style.display = "block";
+}
